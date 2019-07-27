a8363ef3ba25c1a0a90ca5facf36dc875a57eef4
Aurelien <linux.aurelien@gmail.com>
2019-07-27 12:14:53 +0200

    fin de la leçon 56

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Ad;
+use App\Form\AnnonceType;
 use App\Repository\AdRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -28,22 +30,39 @@ class AdController extends AbstractController
     }
 
     /**
+     * Permet de creer une annoce
+     *
+     * @Route("/ads/new", name="ads_create")
+     * 
+     * @return Response
+     */
+    public function create()
+    {
+        $ad = new Ad();
+        
+        $form = $this->createForm(AnnonceType::class, $ad);
+
+        return $this->render('ad/new.html.twig',[
+            'form' => $form->createView()
+        ]);
+    }
+
+
+    /**
      * Permet d'afficher une seule annonce
      *
      * @Route("/ads/{slug}", name="ads_show")
      * 
      * @return Response
      */
-    public function show($slug, AdRepository $repo){
+    public function show(Ad $ad){
         
         //je récupere l'annonce qui correspond au slug !
-        
-        $ad = $repo->findOneBySlug($slug);
+        //$ad = $repo->findOneBySlug($slug);
 
         return $this->render('ad/show.html.twig', [
             'ad'=>$ad
         ]);
     }
-
 
 }
---
+++ src/Form/AnnonceType.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Ad;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+
+class AnnonceType extends AbstractType
+{
+    /**
+     * Permet d'avoir la config de base d'un champ !
+     *
+     * @param string $label
+     * @param string $placeholder
+     * @return array
+     */
+    private function getConfiguration($label,$placeholder) {
+        return [
+            'label'=>$label,
+            'attr' => [
+                'placeholder'=>$placeholder
+            ]
+        ];
+    }
+
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class, $this->getConfiguration('Titre','entrez votre super titre'))
+            ->add('slug', TextType::class,$this->getConfiguration('Adresse Web','tapez l\'adresse web (automatique)'))
+            ->add('coverImage', UrlType::class, $this->getConfiguration('URL de l\'image',"Donnez l'adresse d'une image qui donne vraiment envie"))
+            ->add('introduction', TextType::class, $this->getConfiguration('Introduction',"Donnez une description globale de l'annonce"))
+            ->add('content', TextareaType::class, $this->getConfiguration('Description détaillé',"Donnez une description qui donne des précisions pour avoir envie de venir chez vous"))
+            ->add('rooms',IntegerType::class, $this->getConfiguration('Nombre de chambres',"le nombres de chambres disponible"))
+            ->add('price', MoneyType::class,$this->getConfiguration('Prix par nuit','Indiquez le prix par nuit') )
+            
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Ad::class,
+        ]);
+    }
+}

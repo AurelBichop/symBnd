4f14fe23e690324e652ac0c88e2a06b9d59f48d9
Aurelien <linux.aurelien@gmail.com>
2019-08-22 16:56:56 +0200

    Page d'acceuil en place avec les meilleurs annonce et proprio

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\AdRepository;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -9,40 +11,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController {
 
-
-/**
- * Montre la page et dit hello au prenom entré dans l'url
- *
- * @Route("/hello/{prenom}/age/{age}",name="hello", requirements={"age"="\d+"})
- * @Route("/hello",name="hello_base")
- * @Route("/hello/{prenom}",name="hello_prenom")
- */
-    public function hello($prenom ="anonyme", $age=0){
-        return $this->render(
-            'hello.html.twig',
-            [
-                'prenom'=>$prenom,
-                'age'=>$age
-            ]
-
-        );
-    }
-
     /**
      * @Route("/",name="homepage")
      *
      */
-    public function home(){
-
-        $prenoms = ['lior'=>31,'jean'=>55, 'aure'=>12];
-
+    public function home(AdRepository $adRepo, UserRepository $userRepo){
 
         return $this->render(
             'home.html.twig',
             [
-                'title'=>'Bonjour title',
-                'age'=>12,
-                'tableau'=>$prenoms
+                'ads'=>$adRepo->findBestAds(3),
+                'users'=>$userRepo->findBestUsers()
             ]
         );
     }
